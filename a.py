ce9ea66184acb826431b05d1e1ce9dbb89b8f874
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2021-12-06 19:33:01 +0530

    deletefilepython

---
+++ a.py
@@ -0,0 +1,6 @@
+# lets define a function
+def remove():
+    print("the file has been removed")
+
+#call the function
+#remove()
---
+++ filedelete.py
@@ -0,0 +1,10 @@
+#example of predefine module
+import os # os is operting system
+
+#example of user defined module
+import a
+
+
+os.remove('c.txt')
+
+a.remove()
